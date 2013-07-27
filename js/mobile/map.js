7e61d02781ef1a05a4d0591dd454046c1152b210
kajjjak <mail@kjartanjonsson.com>
2013-07-27 06:28:43 -0700

    Initial backup

---
+++ js/mobile/map.js
@@ -0,0 +1,119 @@
+mapUtils = function() {
+
+var MAP, MAP_CONTROL, BASE_LAYERS = {};
+
+var ATTRIBUTION = 'Map data &copy; '
+  + '<a href="http://openstreetmap.org">OpenStreetMap</a> contributors, '
+  + '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, '
+  + 'Imagery &copy; MapBox';
+
+
+/// https://github.com/ismyrnow/Leaflet.functionaltilelayer
+function tilePath(fileSystem, mapID) {
+    var rootDir = fileSystem.root.fullPath;
+    if (rootDir[rootDir.length-1] != '/') { rootDir += '/'; }
+    if(true){
+        return "http://api.tiles.mapbox.com/v3/" + mapID + '/{z}/{x}/{y}.png';
+    }
+    else {return rootDir + 'tiles/' + mapID + '/{z}/{x}/{y}.png';}
+}
+
+function reloadMap(options) {
+    /*
+     * Clear layers and reset based
+     * on current mapIDs
+     */
+     
+    //handle options
+    var clear = options['clear'] || false;
+    var fileSystem = options['fileSystem'] || null;
+    var mapboxIDs = options['mapIDs'] || null;
+    
+    if (!fileSystem) { alert('Must specify fileSystem'); return; }
+
+    if (!MAP) { //initialize map if first time
+        MAP = L.map('map', {
+        	'zoomControl':false,
+            'minZoom': MAP_MIN_ZOOM,
+            'maxZoom': MAP_MAX_ZOOM,
+            'layers': [funcLayer]
+        }).setView([64.1404809, -21.9113811], MAP_DEFAULT_ZOOM);
+        
+        MAP.locate({setView: true}); //, maxZoom: MAP_MAX_ZOOM
+		function onLocationFound(e) {
+			var radius = e.accuracy / 2;
+			//L.marker(e.latlng).addTo(map).bindPopup("You are within " + radius + " meters from this point").openPopup();
+			if (window.user_marker == undefined){
+				window.user_marker = L.marker(e.latlng);
+				window.user_marker.addTo(map);
+				window.user_circle = L.circle(e.latlng, radius);
+				window.user_circle.addTo(map);
+			} else {
+				window.user_marker.setLatLng(e.latlng);
+				window.user_circle.setLatLng(e.latlng);
+				window.user_circle.setRadius(radius);
+			}
+			locateNearbyMarkers(e.latlng);
+		}
+
+		function onLocationError(e) {
+			showLoadingAnimation("GPS staðsettning: " + e.message, 4000);
+		}
+
+		MAP.on('locationfound', onLocationFound);
+		MAP.on('locationerror', onLocationError);
+
+		MAP.locate({setView: true, watch: true, enableHighAccuracy: true});
+        
+    }
+    window.map = MAP;
+    
+	window.resizeTimer;
+	$(window).resize(function() {
+		clearTimeout(window.resizeTimer);
+		window.resizeTimer = setTimeout(resizeMap, 100);
+	});    
+/*        
+    //clear out old MAP_CONTROL
+    if (MAP_CONTROL) { 
+        MAP.removeControl(MAP_CONTROL); 
+        MAP_CONTROL=null; 
+    }
+
+    //clear out old layers
+    for (mapID in BASE_LAYERS) {
+        var lyr = BASE_LAYERS[mapID];
+        lyr.redraw(); //clear tiles
+        MAP.removeLayer(lyr);
+    }
+    BASE_LAYERS = {};
+    
+    if (clear) { return; } //job done if just clear
+    
+    if (mapboxIDs == null) { return; } //no ids
+    
+    //add a layer for each mapbox ID
+
+    for (var i=0, l=mapboxIDs.length; i<l; i++) {
+        var mapID = mapboxIDs[i];
+        var lyr = L.tileLayer(
+            tilePath(fileSystem, mapID), 
+            {
+	            attribution: ATTRIBUTION,
+	            minZoom: 3,
+	            maxZoom: 17
+            }
+        );
+        BASE_LAYERS[mapID] = lyr;
+        lyr.addTo(MAP); 
+    }
+    
+    MAP_CONTROL = L.control.layers(BASE_LAYERS, {});
+    MAP_CONTROL.addTo(MAP);
+*/}
+
+return {
+    'reloadMap': reloadMap
+};
+
+}();
