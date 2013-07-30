3e59fd611b0ea04f97c13b8aa0f5ef0da40256f9
kajjjak <mail@kjartanjonsson.com>
2013-07-29 23:16:46 -0700

    Test

--- js/mobile/map.js
+++ js/mobile/map.js
@@ -7,7 +7,6 @@ var ATTRIBUTION = 'Map data &copy; '
   + '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, '
   + 'Imagery &copy; MapBox';
 
-
 /// https://github.com/ismyrnow/Leaflet.functionaltilelayer
 function tilePath(fileSystem, mapID) {
     var rootDir = fileSystem.root.fullPath;
@@ -73,44 +72,7 @@ function reloadMap(options) {
 		clearTimeout(window.resizeTimer);
 		window.resizeTimer = setTimeout(resizeMap, 100);
 	});    
-/*        
-    //clear out old MAP_CONTROL
-    if (MAP_CONTROL) { 
-        MAP.removeControl(MAP_CONTROL); 
-        MAP_CONTROL=null; 
-    }
-
-    //clear out old layers
-    for (mapID in BASE_LAYERS) {
-        var lyr = BASE_LAYERS[mapID];
-        lyr.redraw(); //clear tiles
-        MAP.removeLayer(lyr);
-    }
-    BASE_LAYERS = {};
-    
-    if (clear) { return; } //job done if just clear
-    
-    if (mapboxIDs == null) { return; } //no ids
-    
-    //add a layer for each mapbox ID
-
-    for (var i=0, l=mapboxIDs.length; i<l; i++) {
-        var mapID = mapboxIDs[i];
-        var lyr = L.tileLayer(
-            tilePath(fileSystem, mapID), 
-            {
-	            attribution: ATTRIBUTION,
-	            minZoom: 3,
-	            maxZoom: 17
-            }
-        );
-        BASE_LAYERS[mapID] = lyr;
-        lyr.addTo(MAP); 
-    }
-    
-    MAP_CONTROL = L.control.layers(BASE_LAYERS, {});
-    MAP_CONTROL.addTo(MAP);
-*/}
+}
 
 return {
     'reloadMap': reloadMap
