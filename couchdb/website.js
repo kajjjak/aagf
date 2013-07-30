6a355802223f6ed2d150ae4e11a3f764b3d3767c
kajjjak <mail@kjartanjonsson.com>
2013-07-29 23:17:53 -0700

    Test

---
+++ couchdb/website.js
@@ -0,0 +1,18 @@
+function(doc, req){
+
+	var Mustache = require('vendor/codifyr/lib/mustache.js');
+	if (!doc || doc.type != "article") {return "404";} //{return {"code": 302, "body": "See other", "headers": {"Location": "/"}};}
+	var map_content = "";
+	if (doc.map){
+	    doc["map_content"] = Mustache.to_html(this.templates.website_map, doc);
+	}
+	
+	var is_admin = false;
+	var admin_tools = "";
+	//if (req.userCtx.roles.indexOf("_admin") > -1){ is_admin = true; }
+	if (req.userCtx.roles.indexOf("author") > -1){ is_admin = true; }
+	if (is_admin){
+	    doc["admin_tools"] = Mustache.to_html(this.templates.website_editor, doc);
+	}
+	return Mustache.to_html(this.templates.website, doc);
+}
